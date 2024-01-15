8adafa21f2f483f4484860f32e58d872a879f323
FedericaSidoti <federicasidoti21@gmail.com>
2024-01-15 15:14:17 +0100

    ADD- eager loading

--- src/views/projects/index.vue
+++ src/views/projects/index.vue
@@ -29,10 +29,31 @@
 
 <template>
 
-    <div v-for="project in projects">
-    <p>{{ project.title }}</p></div>
+    <div class="card" v-for="project in projects">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.type.name }}</p>
+        <p>{{ project.type.description }}</p>
+        <div class="tags">
+            <p v-for="tech in project.technologies">{{ tech.name }}</p>
+        </div>
+        
+    </div>
+
 </template>
 
 <style lang="scss">
+
+.card {
+    display: flex; 
+    border: 1px solid black;
+    flex-direction:column; 
+    gap: 10px; 
+    padding: 10px; 
+}
+
+.tags {
+    display: flex;
+    gap: 10px; 
+}
 
 </style>
